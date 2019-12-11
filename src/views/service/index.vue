c38c67d032b936ed959f78ab79ef48a22604e587
zhouyaling <1006911224@qq.com>
2019-12-11 18:24:19 +0800

    服务不一样页面

--- src/views/service/index.vue
+++ src/views/service/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="view">
-    <img class="t-banner" src="../../assets/image/sy_t.jpg">
+    <img class="t-banner" src="@/assets/image/service.jpg">
+    <div class="list-box">
+      <ul class="list">
+        <li>专设驻点服务</li>
+        <li>专属产品服务</li>
+        <li>专属理赔通道</li>
+        <li>全国道路救援</li>
+        <li>代办检审服务</li>
+        <li>定期安检服务</li>
+        <li>“车管家”服务</li>
+        <li>索赔助理服务</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -22,6 +34,23 @@ export default {
   .t-banner {
     display: block;
     width: 100%;
-    height: 32vw;
+    height: 33%;
+  }
+
+  .list-box{
+    margin: 2% 10%;
+    display: inline-block;
+    font-size: 1.8rem;
+    color:#006666;
+  }
+
+  .list li{
+    list-style: none;
+    width: 50%;
+    float: left;
+    text-align: center;
+    margin: 1rem 0;
+    border: 1px solid;
+    padding: 0.6rem 0;
   }
 </style>
